f6e0b8c4db47a95f9026cdd632f246643ce671cd
Kumar Anurag <kmranrg@gmail.com>
2018-07-15 23:50:02 +0530

    added fruits in store printing functionality

--- fruit.py
+++ fruit.py
@@ -17,12 +17,30 @@ def addFruit():
         print("\t*** Fruit(s) added successfully ***")
         return
 
+def dataLoad(file):
+        var = pickle.load(file)
+        print(var)
+        return True
+
+def showFruit():
+        file = open("fruitData.pkl","rb")
+        print("\n--------------------------------------------------------\n\t")
+        try:
+                while dataLoad(file):
+                        continue
+        except EOFError:
+                print("\n----- All Fruits in Store are printed successfully -----")
+        file.close()
+        return
+
 while True:
-        ch = int(input("\n\n\tMAIN MENU\n\n1. Add Fruit to Store\n2. Shut Down Application\n\nEnter your choice:"))
+        ch = int(input("\n\n\t--> Main Menu\n\n1. Add Fruit to Store\n2. Show Fruits in Store\n3. Shut Down Application\n\nEnter your choice:"))
 
         if ch == 1:
                 addFruit()
-        elif ch ==2:
+        elif ch == 2:
+                showFruit()
+        elif ch == 3:
                 break
         else:
                 print("Invalid Choice!")
